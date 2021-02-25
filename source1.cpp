f4601878160ee516c580e6f2680f6130c2dad02a
Sam K <kinsellasam@yahoo.com>
2021-02-25 00:49:34 -0600

    asdf

--- source1.cpp
+++ source1.cpp
@@ -3,7 +3,12 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
+
+  char commandLineArg1 = argv[1];
+  if(commandLineArg1 == '-h')
+    cout << "help maessage goes here" << endl;
+    
   cout << "Hello, World!" << endl;
   return 0;
 }
