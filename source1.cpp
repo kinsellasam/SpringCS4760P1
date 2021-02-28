d8d57f31280ad0d20afc40a010cadc35c22522aa
Sam K <kinsellasam@yahoo.com>
2021-02-27 23:43:06 -0600

    TRY THIS!!! FIREBAAAALLLLLLLLLLLLL!!!!

--- source1.cpp
+++ source1.cpp
@@ -1,9 +1,19 @@
 // source1.cpp
+#include <stdlib.h>
 #include <iostream> // header in standard library
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-  cout << "Hello, World!" << endl;
+    cout << "Hi, just running a quick check.";
+    cout << " you typed in " << argc << " arguments:\n";
+
+    for(int i = 0, i < argc; i++)
+      cout << argv[i] << "\n";
+
+    if(argv[1] == "-h"){
+      cout << "The help function was called." << endl;
+      exit(EXIT_SUCCESS);
+    }
   return 0;
 }
