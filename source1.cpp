257a108cb939ebf8064d46876e0b4258f3c293a0
Sam K <kinsellasam@yahoo.com>
2021-02-27 22:34:47 -0600

    f

--- source1.cpp
+++ source1.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int main(int argc, char *argv[]){
 
   char commandLineArg1 = argv[1];
-  if(commandLineArg1 == '-h')
-    cout << "help maessage goes here" << endl;
-    
+  if(commandLineArg1 == 'h'){
+    cout << "help message goes here" << endl;
+    return -1;
+  }
   cout << "Hello, World!" << endl;
   return 0;
 }
