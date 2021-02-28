e7346381ac6d90946ec3fa27a2d46fe7a61abe08
Sam K <kinsellasam@yahoo.com>
2021-02-28 00:53:17 -0600

    dasf

--- source1.cpp
+++ source1.cpp
@@ -16,10 +16,23 @@ int main(int argc, char *argv[]){
       cout << "The help function was called." << endl;
       exit(EXIT_SUCCESS);*/
 
-      if(string(argv[1]) =="-h" || string(argv[1]) == "--help"){    //basic help function
-        cout << "Someone calling for help?" << endl;
+      if(string(argv[1]) =="-h" || string(argv[1]) == "[-h]"){    //basic help function
+        cout << "\n\nSomeone calling for help?" << endl;
+        cout << "This program is goint to comput the sum of integers using";
+        cout << "a BINARY TREE OF PROCESSES.\n";
+        cout << "This program will make use of multiple processes using";
+        cout << "shared memory and signals to communicate between processes\n";
         cout << "Exiting...\n\n\n";
         return 0;
+      }
+
+      if(string(argv[1]) == "-s" && argv[2] >= 0){
+        //indicate the number of children alowed to exist
+        //default: 20
+        const int defaultChildren = 20;
+
+        int userInput = argv[2];
+        int childCount = 0;
       }
 
     }
