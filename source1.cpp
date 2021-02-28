d2dde67b542face4758a5d90fb58b6d1db28f31a
Sam K <kinsellasam@yahoo.com>
2021-02-28 00:19:07 -0600

    fsdg

--- source1.cpp
+++ source1.cpp
@@ -8,23 +8,18 @@ int main(int argc, char *argv[]){
     cout << "Hi, just running a quick check.";
     cout << " you typed in " << argc << " arguments:\n";
 
-    for(int i = 0; i < argc; i++){
+    for(int i = 0; i < argc; i++){    //checking the user input for errors
       cout << argv[i] << "\n";
 
     /*if(argv[1] == "-h"){
       cout << "The help function was called." << endl;
       exit(EXIT_SUCCESS);*/
 
-      switch(argv[1] =="-h" || argv[0] == "--help"){
+      if(argv[1] =="-h" || argv[1] == "--help"){    //basic help function
+        cout << "Someone calling for help?" << endl;
 
-        case "-h":
-          cout << "Someone calling for help?" << endl;
-          break;
-
-        default:
-          cout << "This expression: " << argv[1]
-               << "Yeah..... I got nothing." << endl;
-          break;
+        cout << "Exiting...\n\n\n";
+        return 0;
       }
 
     }
