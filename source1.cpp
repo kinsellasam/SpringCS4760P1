60650d13930435dc79913541bd036345727b00e9
Sam K <kinsellasam@yahoo.com>
2021-02-27 23:10:40 -0600

    cleanslate

--- source1.cpp
+++ source1.cpp
@@ -4,22 +4,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  int counter;
-  printf("\nThe program name is: %s",argv[0]);
-  if(argc == 1){
-    printf("You did not put anything else in the command line.");
-    cout << "Allocating memory...\n\n";
-  }
-  if(argc >2){
-    if(argv[1] == "-h"){
-      cout <<"The help command has been called.\n");
-      cout <<"This program is designed to compute the sum of n integers" &
-             " using a binary tree of processes." << endl;
-      cout <<"Multiple processes will be " &
-             "implemented using shared memory."<< endl;
-    }
-  }
-
-
+  cout << "Hello, World!" << endl;
   return 0;
 }
