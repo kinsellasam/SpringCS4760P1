6bac6b85240b88b0f714933eac58258ab9370b3d
Sam K <kinsellasam@yahoo.com>
2021-02-28 20:53:25 -0600

    flaks;dj

--- source1.cpp
+++ source1.cpp
@@ -82,80 +82,57 @@ int main(int argc, char *argv[]){
         argumentCount++;
       }
 
-    case 1:
-      if(string(argv[argumentCount]) == "-h"){    //basic help function
-        cout << "Current Argument: " << argv[argumentCount];
-        cout << "\nSomeone calling for help?" << endl;
-        cout << "This program is going to compute the sum of integers using";
-        cout << " a BINARY TREE OF PROCESSES.\n";
-        cout << "This program will make use of multiple processes using";
-        cout << " shared memory and signals to communicate between processes\n";
-        cout << "Exiting...\n\n\n";
-        argumentCount = argumentCount + 1;
-        return 0;
-      }
-      else if(string(argv[argumentCount]) == "-s"){
-
-        int numOfChildren = atoi(argv[argumentCount+1]);
-        if(numOfChildren > DEFAULT_CHILDREN){
-          cout << "You said you wanted " << numOfChildren << endl;
-          cout << "That is over the maximum.\n";
-          cout << "The maximum number allowed is 20. So let's use that...\n";
-          numOfChildren = DEFAULT_CHILDREN;
-        }
-        else if(numOfChildren < 0){
-          cout << numOfChildren << " ..... Really?\n";
-          cout << "The maximum number allowed is 20. So let's use that...\n";
-          numOfChildren = DEFAULT_CHILDREN;
-        }
-        else{
-          cout << "The number of children is: " << numOfChildren << endl;
-          cout << "Argument: " << argv[argumentCount];
-          cout << "\nArgument: " << argv[argumentCount+1] << "\n";
-          cout << "This is where the forking action happens!" << endl;
-          cout << "Forking " << argv[argumentCount+1] << " children." << endl;
-          argumentCount = argumentCount + 2;
-          break;
-
-        }
-
-
-
-      }
-      else if(string(argv[argumentCount]) == "-s" &&
-              string(argv[argumentCount+1]) == "x"){ //indicate the number of children alowed to exist default: 20
-
-
-        cout << "Argument: " << argv[argumentCount];
-        cout << "\nArgument: " << argv[argumentCount+1] << "\n";
-        cout << "By default, only 20 children "
-             << "are allowed to exist at a time.\n" << endl;
-        argumentCount = argumentCount + 2;
-        break;
-      }
-      else if(string(argv[argumentCount]) == "-t"){
-
-        if(argc == 2){
-          cout << "\nThe command you're looking for is '-t time'" << endl;
-          argumentCount++;
+      case 1:
+        if(string(argv[argumentCount]) == "-h"){    //basic help function
+          cout << "Current Argument: " << argv[argumentCount];
+          cout << "\nSomeone calling for help?" << endl;
+          cout << "This program is goint to compute the sum of integers using";
+          cout << " a BINARY TREE OF PROCESSES.\n";
+          cout << "This program will make use of multiple processes using";
+          cout << " shared memory and signals to communicate between processes\n";
+          cout << "Exiting...\n\n\n";
+          argumentCount = argumentCount + 1;
           return 0;
         }
-        else{
-            if(string(argv[argumentCount+1]) == "time"){
-              cout << "Argument: " << argv[argumentCount];
-              cout << "\nArgument: " << argv[argumentCount+1] << "\n";
-              cout << "By default, the program will terminate";
-              cout << " after 100 seconds.\n\n";
-              argumentCount = argumentCount + 2;
-              break;
-            }
-            else{
-              cout << "\nThe command you're looking for is '-t time'" << endl;
-              argumentCount++;
-              return 0;
+        else if(string(argv[argumentCount]) == "-s" &&
+                string(argv[argumentCount+1]) == "i"){
+                  cout << "Argument: " << argv[argumentCount];
+                  cout << "Argument: " << argv[argumentCount+1] << "\n";
+                  cout << "This is where the forking action happens!\n\n" << endl;
+                  argumentCount = argumentCount + 2;
+                  break;
+        }
+        else if(string(argv[argumentCount]) == "-s" &&
+                string(argv[argumentCount+1]) == "x"){
+                  cout << "Argument: " << argv[argumentCount];
+                  cout << "\nArgument: " << argv[argumentCount+1] << "\n";
+                  cout << "By default, only 20 children "
+               << "are allowed to exist at a time.\n\n" << endl;
+          argumentCount = argumentCount + 2;
+          break;
+        }
+        else if(string(argv[argumentCount]) == "-t"){
+          if(argc == 2){
+            cout << "\nThe command you're looking for is '-t time'" << endl;
+            argumentCount++;
+            return 0;
+          }
+          else{
+              if(string(argv[argumentCount+1]) == "time"){
+                cout << "Argument: " << argv[argumentCount];
+                cout << "\nArgument: " << argv[argumentCount+1] << "\n";
+                cout << "By default, the program will terminate after 100 seconds.\n\n";
+                argumentCount = argumentCount + 2;
+                return 0;
+              }
+              else{
+                cout << "\nThe command you're looking for is '-t time'\n" << endl;
+                argumentCount++;
+                return 0;
+              }
             }
           }
-        }
+
 
     case 3:
       if(string(argv[argumentCount]) == "-h"){    //basic help function
