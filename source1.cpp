ccec516ea4e33d1a975f10b555c1fde26d8efc83
Sam K <kinsellasam@yahoo.com>
2021-03-01 00:39:33 -0600

    asdf

--- source1.cpp
+++ source1.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]){
 
   bool done = false;
   while (true){
-		int c = getopt(argc, argv, "h:st:");
+		int c = getopt(argc, argv, ":h:st:");
 
 		if(c == -1){
       cout << "Hi. Try running ./master -h\n";
